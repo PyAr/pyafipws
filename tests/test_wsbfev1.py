ae26e6a3baeda8c47650a959fc559fe2d100198b
Sando <nicolassandovalunm@gmail.com>
2019-07-19 13:42:20 -0300

    Agrego cambios al test_wsbfev1.py

--- tests/test_wsbfev1.py
+++ tests/test_wsbfev1.py
@@ -22,6 +22,18 @@ from datetime import datetime, timedelta
 from pyafipws.wsaa import WSAA
 from pyafipws.wsbfev1 import WSBFEv1
 
+cert = os.environ['CERT']
+pkey = os.environ['PKEY']
+
+CERT = cert.replace(r'\n', '\n')
+PKEY = pkey.replace(r'\n', '\n')
+
+
+with open('rei.crt', 'w', encoding='utf-8') as f:
+    f.write(CERT)
+    
+with open('rei.key', 'w', encoding='utf-8') as f:
+    f.write(PKEY)
 
 WSDL = "https://wswhomo.afip.gov.ar/wsbfev1/service.asmx?WSDL"
 CUIT = os.environ['CUIT']
