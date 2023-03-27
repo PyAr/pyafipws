56db255245f3ec856d3403b44c6c78f66af6473a
HanslettTheDev <HanslettTheDev@gmail.com>
2023-03-27 15:14:33 +0100

    feat: get all required dependencies in setup.py from issue #105
    Signed-off-by: HanslettTheDev <HanslettTheDev@gmail.com>
    
    fix: improved the get_dependencies function to grab all dependency modules
    
    Signed-off-by: HanslettTheDev <HanslettTheDev@gmail.com>
    
    fix: improved the get_dependencies function to grab all dependency modules; commit signed
    
    Signed-off-by: HanslettTheDev <HanslettTheDev@gmail.com>
    
    fix: fixed the issue with pip not detecting links
    
    Signed-off-by: HanslettTheDev <HanslettTheDev@gmail.com>
    
    fix: removed the python version number in the requirements.txt file
    fix: fixed the error when replacing the links with the module name
    The overall errors fixed was to prevent the setup.py build from failing
    
    Signed-off-by: HanslettTheDev <HanslettTheDev@gmail.com>
    
    feat: get all dependencies from the requirements.txt file to be used by
    the setup.py build process

--- get_dep.py
+++ get_dep.py
@@ -1,7 +1,24 @@
-import subprocess
+import os
 
-def get_dependecies() -> list:
-    freeze = subprocess.Popen("pip freeze", shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-    g = freeze[0].decode("utf-8").split("\r\n")
-    dependencies = [">=".join(x.split("==")) for x in g]
-    return dependencies
+
+def get_dependecies():
+    blob = "git+https://github.com"
+    requirements_path = os.path.join(
+        os.path.abspath(os.getcwd()),
+        "requirements.txt"
+    )
+    if os.path.isfile(requirements_path):
+        with open(requirements_path) as f:
+            dependencies = [
+                ">=".join(x.split("==")) for x in f.read().splitlines()
+            ]
+            for x in dependencies:
+                if x.startswith(blob):
+                    # split the text and join them with the @ command
+                    # index 3 holds the name of the module
+                    chunks = x.split("/")
+                    dependencies[dependencies.index(x)] = x.replace(
+                        blob, chunks[3] + " @ " + blob
+                    )
+                    break
+        return dependencies
